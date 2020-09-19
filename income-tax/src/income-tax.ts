00b07683c9a2041f59baef6fb4cd701253d53339
Kazuhiro Kojio <kazuhiro.kojio@gmail.com>
2020-09-18 22:17:42 -0300

    remove useless code

--- income-tax/src/income-tax.ts
+++ income-tax/src/income-tax.ts
@@ -3,16 +3,14 @@
 // se 1000 < salário-base <= 2000, IR = (salário-base - 1000) * 15%
 // se salário-base <= 1000, IR = 0
 
-export function calculateTax (salario: number): number {
-  let ret: number = 0.0
-
-  if (salario > 2000.0) {
-    return (150 + (salario - 2000.0) * (0.20))
+export function calculateTax(salary: number): number {
+  if (salary > 2000.0) {
+    return (150 + (salary - 2000.0) * (0.20))
   }
 
-  if (salario > 1000.0) {
-    return ((salario - 1000.0) * (0.15))
+  if (salary > 1000.0) {
+    return ((salary - 1000.0) * (0.15))
   }
 
-  return ret
+  return 0.0
 }
