bf342f179982cd7d06db5a6a4b3036ca1aa0b18a
Kazuhiro Kojio <kazuhiro.kojio@gmail.com>
2020-09-18 22:52:42 -0300

    add specs to helper

--- income-tax/src/utils/helpers.ts
+++ income-tax/src/utils/helpers.ts
@@ -1,9 +1,3 @@
-export function generateArrayNumbers(
-  size: number,
-  min: number,
-  max: number
-): number[] {
-  return Array.from(
-    Array(size)
-  ).map(() => Math.random() * (max - min) + min)
+export function generateArrayNumbers(size: number, min: number, max: number): number[] {
+  return Array.from(Array(size)).map(() => Math.random() * (max - min) + min)
 }
