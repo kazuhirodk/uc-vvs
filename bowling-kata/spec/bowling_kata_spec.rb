a8b90a792d60578d7412aff640be70e4ba83b3e1
Kazuhiro Kojio <kazuhiro.kojio@gmail.com>
2020-10-18 23:33:01 -0300

    initialize bowling kata and add gutter ball

--- bowling-kata/spec/bowling_kata_spec.rb
+++ bowling-kata/spec/bowling_kata_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe BowlingKata do
+  game = BowlingKata.new
+
   context 'when player got a gutter ball' do
     it 'should score 0' do
-      game = BowlingKata.new
 
       expect(game.score).to eq(0)
+    end
+  end
+
+  context 'when player got a all ones game' do
+    it 'should score 20' do
+
     end
   end
 end
