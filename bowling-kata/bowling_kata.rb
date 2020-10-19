ecdb141fbc57b98b1667fb8d8f5ad7eb159c209e
Kazuhiro Kojio <kazuhiro.kojio@gmail.com>
2020-10-19 01:05:08 -0300

    add specs and adjust methods

--- bowling-kata/bowling_kata.rb
+++ bowling-kata/bowling_kata.rb
@@ -1,13 +1,53 @@
 class BowlingKata
   def initialize
-    @score = 0
-  end
-
-  def roll(pins)
-    @score += pins
+    @rolls = Array.new(21) { 0 }
+    @current_roll = 0
   end
 
   def score
-    return @score
+    score = 0
+    frame_index = 0
+
+    for i in 1..10
+      if (is_strike(frame_index))
+        score += 10 + strike_bonus(frame_index)
+        frame_index += 1
+      elsif (is_spare(frame_index))
+        score += 10 + spare_bonus(frame_index)
+        frame_index += 2
+      else
+        score += sum_of_balls_in_frame(frame_index)
+        frame_index += 2
+      end
+    end
+
+    return score
+  end
+
+  def roll(pins)
+    @rolls[@current_roll] = pins
+    @current_roll += 1
+  end
+
+  private
+
+  def sum_of_balls_in_frame(frame_index)
+    return @rolls[frame_index] + @rolls[frame_index + 1]
+  end
+
+  def strike_bonus(frame_index)
+    return @rolls[frame_index + 1] + @rolls[frame_index + 2]
+  end
+
+  def spare_bonus(frame_index)
+    return @rolls[frame_index + 2]
+  end
+
+  def is_strike(frame_index)
+    return @rolls[frame_index] == 10
+  end
+
+  def is_spare(frame_index)
+    return (@rolls[frame_index] + @rolls[frame_index + 1]) == 10
   end
 end
--- bowling-kata/spec/bowling_kata_spec.rb
+++ bowling-kata/spec/bowling_kata_spec.rb
@@ -1,12 +1,25 @@
 require 'spec_helper'
 
+def roll_many(numGames, numPins, game)
+  for i in 1..numGames
+    game.roll(numPins)
+  end
+end
+
+def roll_spare(game)
+  game.roll(5)
+  game.roll(5)
+end
+
+def roll_strike(game)
+  game.roll(10)
+end
+
 describe BowlingKata do
   context 'when player got a gutter ball' do
     game = BowlingKata.new
 
-    for i in 1..20
-      game.roll(0)
-    end
+    roll_many(20, 0, game)
 
     it 'should score 0' do
       expect(game.score).to eq(0)
@@ -16,12 +29,43 @@ describe BowlingKata do
   context 'when player got a all ones game' do
     game = BowlingKata.new
 
-    for i in 1..20
-      game.roll(1)
-    end
+    roll_many(20, 1, game)
 
     it 'should score 20' do
       expect(game.score).to eq(20)
+    end
+  end
+
+  context 'when player got a spare followed by a 3 ball' do
+    game = BowlingKata.new
+
+    roll_spare(game)
+    game.roll(3)
+
+    it 'should score 16' do
+      expect(game.score).to eq(16)
+    end
+  end
+
+  context 'when player got a strike followed by 3 and a 4 ball' do
+    game = BowlingKata.new
+
+    roll_strike(game)
+    game.roll(3)
+    game.roll(4)
+
+    it 'should score 24' do
+      expect(game.score).to eq(24)
+    end
+  end
+
+  context 'when player got a perfect game' do
+    game = BowlingKata.new
+
+    roll_many(12, 10, game)
+
+    it 'should score 300' do
+      expect(game.score).to eq(300)
     end
   end
 end
